2515f8af8323ba4d5ff357e8af4b68346cf98347
iseyskrrr <iselinils65@gmail.com>
2022-07-08 16:37:36 +0200

    final commit

--- app/src/main/java/ch/zh/zli/mainproject/CallActivity.java
+++ app/src/main/java/ch/zh/zli/mainproject/CallActivity.java
@@ -2,13 +2,72 @@ package ch.zh.zli.mainproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.os.Vibrator;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.Timer;
+import java.util.concurrent.TimeUnit;
 
 public class CallActivity extends AppCompatActivity {
+    TextView callerName;
+    TextView callerNumber;
+    Button decline;
+    Button accept;
+
+    SharedPreferences sharedPreferences;
+    private Vibrator vibrator;
+    MediaPlayer ybBetter;
+
+    private static final String SHARED_PREF_NAME ="mypref";
+    private static final String KEY_NAME ="name";
+    private static final String KEY_NUMBER ="number";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_call);
+
+        callerName = findViewById(R.id.callerName);
+        callerNumber = findViewById(R.id.phoneNumber);
+        decline = findViewById(R.id.decline);
+        accept = findViewById(R.id.accept);
+
+        sharedPreferences = getSharedPreferences(SHARED_PREF_NAME, MODE_PRIVATE);
+
+        String name = sharedPreferences.getString(KEY_NAME,null);
+        String number = sharedPreferences.getString(KEY_NUMBER,null);
+
+        if (name != null || number != null){
+            callerName.setText(name);
+            callerNumber.setText(number);
+        }
+        vibrator = (Vibrator) getSystemService(VIBRATOR_SERVICE);
+        long[] pattern = {0, 100, 1000, 200, 2000};
+        vibrator.vibrate(pattern,  0);
+
+        accept.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finishAndRemoveTask();
+            }
+        });
+        decline.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finishAndRemoveTask();
+            }
+        });
+
+
+
+
+
+
     }
 }
--- app/src/main/java/ch/zh/zli/mainproject/MainActivity.java
+++ app/src/main/java/ch/zh/zli/mainproject/MainActivity.java
@@ -5,61 +5,106 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.os.Handler;
 import android.widget.RadioGroup;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-        EditText callerName;
-        EditText callerNumber;
-        RadioGroup timerDuration;
-        Button callButton;
-        SharedPreferences sharedPreferences;
+    EditText callerName;
+    EditText callerNumber;
+    RadioGroup timerDuration;
+    TextView countDown;
+    Button callButton;
+    SharedPreferences sharedPreferences;
 
         private static final String SHARED_PREF_NAME ="mypref";
         private static final String KEY_NAME ="name";
         private static final String KEY_NUMBER ="number";
 
+        final Handler handler = new Handler();
 
+
+    int timer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        callerName = findViewById(R.id.calllerName);
-        callerNumber = findViewById(R.id.phoneNumber);
+        callerName = findViewById(R.id.callerNameInput);
+        callerNumber = findViewById(R.id.phoneNumberInput);
         callButton = findViewById(R.id.call);
+        countDown = findViewById(R.id.countDown);
+
 
 
         sharedPreferences = getSharedPreferences(SHARED_PREF_NAME, MODE_PRIVATE);
 
         String name = sharedPreferences.getString(KEY_NAME,null);
-        if (name!= null){
-            Intent intent = new Intent(MainActivity.this,CallActivity.class);
-            startActivity(intent);
-        }
 
+        RadioGroup rg = (RadioGroup) findViewById(R.id.radioGroup);
 
-        callButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                SharedPreferences.Editor  editor = sharedPreferences.edit();
-                editor.putString(KEY_NAME,callerName.getText().toString());
-                editor.putString(KEY_NUMBER,callerNumber.getText().toString());
-                editor.apply();
-
-
-                Intent intent = new Intent(MainActivity.this,CallActivity.class);
-                startActivity(intent);
-
-                Toast.makeText(MainActivity.this, "", Toast.LENGTH_SHORT).show();
-
+        rg.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener()
+        {
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                switch(checkedId){
+                    case R.id.firstTimer:
+                        timer = 15;
+                        break;
+                    case R.id.secondTimer:
+                        timer = 30;
+                        break;
+                    case R.id.thirdTimer:
+                        timer = 45;
+                        break;
+                    case R.id.fourthTimer:
+                        timer = 60;
+                        break;
+                }
+                timer *= 1000;
             }
         });
+        callButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                new CountDownTimer(timer, 1000) {
+
+                    public void onTick(long millisUntilFinished) {
+                        countDown.setText("Call in: " + millisUntilFinished / 1000);
+                    }
+
+                    public void onFinish() {
+                    }
+                }.start();
+
+                handler.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        SharedPreferences.Editor  editor = sharedPreferences.edit();
+                        editor.putString(KEY_NAME,callerName.getText().toString());
+                        editor.putString(KEY_NUMBER,callerNumber.getText().toString());
+                        editor.apply();
+
+
+                        Intent intent = new Intent(MainActivity.this,CallActivity.class);
+
+
+                        startActivity(intent);
+
+                    }
+                }, timer);
+            }
+        });
+    }
+    private void threadRunner(){
+
 
     }
 
